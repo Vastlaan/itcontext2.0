fa017b7646e4ec7162baa42499ac5522e35036d3
vastlaan <vastlaan@mail.com>
2020-09-21 12:32:48 +0200

    Implement Cards component and its child Card component

---
+++ client/src/components/webdev/cards/card.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled, { StyledFunction } from "styled-components";
+import { respond, fonts, Text, Button } from "../../../styles";
+import { IconType } from "react-icons";
+
+interface CardProps {
+    btn: string;
+    link: string;
+    hovered?: boolean;
+    translate?: string;
+    image?: string;
+    icon?: JSX.Element;
+    header?: string;
+    text?: string;
+}
+interface CardStyledProps {
+    hovered?: boolean;
+    trans?: string;
+    image?: string;
+}
+
+export default function Card(props: CardProps) {
+    const { btn, link, hovered, translate, image, icon, header, text } = props;
+
+    return (
+        <Container hovered={hovered} trans={translate} image={image}>
+            <Icon hovered={hovered}>{icon}</Icon>
+            <Header hovered={hovered}>{header}</Header>
+            <CardText hovered={hovered}>{text}</CardText>
+            <CardButton>{btn}</CardButton>
+        </Container>
+    );
+}
+
+const Container = styled.div<CardStyledProps>`
+    min-height: 40rem;
+    width: 35rem;
+    padding: 3rem 2rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-radius: 3px;
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.3);
+    background-image: ${(props) =>
+        props.hovered
+            ? `linear-gradient(to bottom, rgba(17, 120, 100,.6), rgba(17, 120, 100,1)), url(${props.image})`
+            : "linear-gradient(to bottom, rgba(246,249,255,1), rgba(246,249,255,1))"};
+    transform: translateY(${(p) => p.trans});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    transition: all 0.3s;
+
+    ${() => respond("m", "width: 25rem;")}
+    ${() => respond("m", "width: 30rem;")}
+
+    &:hover {
+        background-image: ${(p) => `linear-gradient(
+                to bottom,
+                rgba(17, 120, 100, 0.6),
+                rgba(17, 120, 100, 1)
+            ),
+            url(${p.image})`};
+        div,
+        p,
+        svg {
+            color: ${(props) => (props.hovered ? `snow` : `snow`)};
+        }
+    }
+`;
+
+const Icon = styled.div<CardStyledProps>`
+    margin: 0 auto 2rem auto;
+
+    svg {
+        font-size: 5rem;
+        color: ${(props) => (props.hovered ? `snow` : "#117864")};
+        transition: all 0.3s;
+    }
+`;
+const Header = styled.div<CardStyledProps>`
+    font-family: ${fonts.gayathri};
+    font-size: 2.5rem;
+    text-transform: capitalize;
+    text-align: center;
+    transition: all 0.3s;
+    color: ${(props) => (props.hovered ? `snow` : "#0F284E")};
+`;
+const CardText = styled(Text)<CardStyledProps>`
+    color: ${(props) => (props.hovered ? `snow` : "#18191F")};
+    font-size: 1.8rem;
+    text-align: center;
+    transition: all 0.3s;
+`;
+const CardButton = styled(Button)`
+    padding: 0.5rem 2rem;
+    background-color: ${(props) => props.theme.fresh};
+    border: 1px solid ${(props) => props.theme.bg};
+
+    &:hover {
+        color: ${(props) => props.theme.bg};
+    }
+`;
--- client/src/components/webdev/cards/index.tsx
+++ client/src/components/webdev/cards/index.tsx
@@ -2,17 +2,92 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useIntl } from "react-intl";
+import Card from "./card";
 import { respond, fonts } from "../../../styles";
+
+import { GiCheckedShield, GiTechnoHeart, GiChart } from "react-icons/gi";
+import Image1 from "../../../img/image-card-1.png";
+import Image2 from "../../../img/image-card-2.png";
+import Image3 from "../../../img/image-card-3.png";
 
 export default function Cards() {
     const intl = useIntl();
 
-    return <Container>Cards</Container>;
+    return (
+        <div>
+            <Container>
+                <Card
+                    btn="read more"
+                    link="/online-marketing"
+                    hovered={true}
+                    image={Image1}
+                    icon={<GiCheckedShield />}
+                    header="secure environment"
+                    text="Our websites are force to use encrypted protocols so that connection is safe and all the users data are protected. "
+                />
+                <Card
+                    btn="read more"
+                    link="/online-marketing"
+                    hovered={false}
+                    translate={"10%"}
+                    icon={<GiTechnoHeart />}
+                    image={Image3}
+                    header="newest technologies"
+                    text="We follow global trends and use newest, best working solutions"
+                />
+                <Card
+                    btn="read more"
+                    link="/online-marketing"
+                    hovered={false}
+                    translate={"20%"}
+                    icon={<GiChart />}
+                    image={Image2}
+                    header="Grow customers base"
+                    text="Our goal is to increase your audience, drag more visitors and convert them in your customers."
+                />
+            </Container>
+            <Container>
+                <Card
+                    btn="read more"
+                    link="/online-marketing"
+                    hovered={false}
+                    image={Image1}
+                    icon={<GiCheckedShield />}
+                    header="secure environment"
+                    text="Our websites are force to use encrypted protocols so that connection is safe and all the users data are protected. "
+                />
+                <Card
+                    btn="read more"
+                    link="/online-marketing"
+                    hovered={true}
+                    translate={"10%"}
+                    icon={<GiTechnoHeart />}
+                    image={Image3}
+                    header="newest technologies"
+                    text="We follow global trends and use newest, best working solutions"
+                />
+                <Card
+                    btn="read more"
+                    link="/online-marketing"
+                    hovered={false}
+                    translate={"20%"}
+                    icon={<GiChart />}
+                    image={Image2}
+                    header="Grow customers base"
+                    text="Our goal is to increase your audience, drag more visitors and convert them in your customers."
+                />
+            </Container>
+        </div>
+    );
 }
 
 const Container = styled.section`
-    margin: 20rem auto;
+    margin: 10rem auto 20rem auto;
     width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
 
-    ${() => respond("m", "width: 90%;")}
+    ${() => respond("m", "width: 90%; flex-direction: row;")}
 `;
