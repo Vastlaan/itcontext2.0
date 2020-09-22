9ad9232e8bb6bbfe1c67e5335d25591858e9cd6f
vastlaan <vastlaan@mail.com>
2020-09-22 15:39:12 +0200

    Implement paragraph in webdev section

--- client/src/components/webdev/index.tsx
+++ client/src/components/webdev/index.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { fonts, PageNav } from "../../styles";
 import { useIntl } from "react-intl";
 import Cards from "./cards";
+import Paragraph from "../paragraph";
 import Intersection from "../intersection";
 import InfoStrip from "../infoStrip";
 import Header from "./header";
@@ -25,6 +26,24 @@ export default function Webdev() {
                 icon={<FaHandshake />}
             />
             <Cards />
+
+            <Paragraph
+                heading="ipsum dolor sit"
+                para1="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus autem veritatis numquam, quidem obcaecati doloremque
+                soluta eveniet. Harum sapiente impedit illo distinctio non
+                error. Iure provident eos illo totam?"
+                para2="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus autem veritatis numquam, quidem obcaecati doloremque
+                soluta eveniet. Harum sapien?"
+                para3="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus autem veritatis numquam, quidem obcaecati doloremque
+                soluta eveniet. Harum sapiente impedit illo distinctio non
+                error. Iure provident eos illo totam?Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus autem veritatis numquam, quidem obcaecati doloremque
+                soluta eveniet. Harum sapiente impedit illo distinctio non
+                error. Iure provident eos illo totam?"
+            />
             <Intersection
                 headline={intl.formatMessage({
                     id: "webdev.intersection1-headline",
@@ -36,6 +55,23 @@ export default function Webdev() {
                         "Onze doel is om betaalbaar, goede kwaliteit websites aan allerlei ondernemingen te leveren. Met eigen e-mailadres, SEO plan en Content Manager System.",
                 })}
                 background={false}
+            />
+            <Paragraph
+                heading="ipsum dolor sit"
+                para1="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus autem veritatis numquam, quidem obcaecati doloremque
+                soluta eveniet. Harum sapiente impedit illo distinctio non
+                error. Iure provident eos illo totam?"
+                para2="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus autem veritatis numquam, quidem obcaecati doloremque
+                soluta eveniet. Harum sapien?"
+                para3="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus autem veritatis numquam, quidem obcaecati doloremque
+                soluta eveniet. Harum sapiente impedit illo distinctio non
+                error. Iure provident eos illo totam?Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus autem veritatis numquam, quidem obcaecati doloremque
+                soluta eveniet. Harum sapiente impedit illo distinctio non
+                error. Iure provident eos illo totam?"
             />
         </Container>
     );
