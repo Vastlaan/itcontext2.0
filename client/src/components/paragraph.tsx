6d35b4e51c86d5154f400cc91f31232195d0c493
vastlaan <vastlaan@mail.com>
2020-09-22 15:38:28 +0200

    Change font spacing and size

--- client/src/components/paragraph.tsx
+++ client/src/components/paragraph.tsx
@@ -3,10 +3,10 @@ import styled from "styled-components";
 import { fonts, respond, Text } from "../styles";
 
 interface ParagraphProps {
-    heading: string;
-    para1: string;
-    para2: string;
-    para3: string;
+    heading?: string;
+    para1?: string;
+    para2?: string;
+    para3?: string;
 }
 
 export default function Paragraph(props: ParagraphProps) {
@@ -15,9 +15,15 @@ export default function Paragraph(props: ParagraphProps) {
     return (
         <Container>
             <Heading>{heading}</Heading>
-            <Text style={{ gridArea: "para1" }}>{para1}</Text>
-            <Text style={{ gridArea: "para2" }}>{para2}</Text>
-            <Text style={{ gridArea: "para3" }}>{para3}</Text>
+            <Text style={{ gridArea: "para1", letterSpacing: ".1rem" }}>
+                {para1}
+            </Text>
+            <Text style={{ gridArea: "para2", letterSpacing: ".1rem" }}>
+                {para2}
+            </Text>
+            <Text style={{ gridArea: "para3", letterSpacing: ".1rem" }}>
+                {para3}
+            </Text>
         </Container>
     );
 }
@@ -28,6 +34,7 @@ const Container = styled.article`
     position: relative;
     width: 100%;
     display: grid;
+    grid-gap: 2rem;
     grid-template-columns: 1fr;
 
     ${() =>
@@ -44,7 +51,7 @@ const Container = styled.article`
         content: "";
         position: absolute;
         top: 0;
-        left: 0;
+        left: 2rem;
         width: 20rem;
         height: 3px;
         background-color: ${(p) => p.theme.secondary};
@@ -54,4 +61,5 @@ const Heading = styled.div`
     font-family: ${fonts.gayathri};
     font-size: 3rem;
     color: ${(p) => p.theme.secondary};
+    text-transform: uppercase;
 `;
