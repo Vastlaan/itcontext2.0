c85198a8668b3a978c88288e3f8429eabfe9a7b3
vastlaan <vastlaan@mail.com>
2020-09-23 17:58:26 +0200

    Add text and translation for first paragraph in web design

--- client/src/components/webdev/index.tsx
+++ client/src/components/webdev/index.tsx
@@ -16,34 +16,46 @@ export default function Webdev() {
     return (
         <Container>
             <Header />
-            <InfoStrip
-                text="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Temporibus autem veritatis numquam, quidem obcaecati doloremque
-                soluta eveniet. Harum sapiente impedit illo distinctio non
-                error. Iure provident eos illo totam?"
-                btn="Find your solution"
-                note="with IT Context"
-                icon={<FaHandshake />}
+            <PageNav>
+                <p>
+                    <Link to="/">
+                        {intl.formatMessage({
+                            id: "webdev.pagenav-home",
+                            defaultMessage: "Voorpagina",
+                        })}
+                    </Link>
+                    {" > "}
+                    <Link to="/website-ontwerpen">
+                        {intl.formatMessage({
+                            id: "webdev.pagenav-current",
+                            defaultMessage: "Website Ontwerpen",
+                        })}
+                    </Link>
+                </p>
+            </PageNav>
+            <Paragraph
+                heading={intl.formatMessage({
+                    id: "webdev.paragraph-1-header",
+                    defaultMessage: "kosten website maken",
+                })}
+                para1={intl.formatMessage({
+                    id: "webdev.paragraph-1-text-1",
+                    defaultMessage:
+                        "Veel ondernemers vragen zich af wat zijn de kosten van professionele website? Om dit vraag antwoord te geven moet er juist nog een vraag gesteld worden. Wat voor heb u een website nodig?",
+                })}
+                para2={intl.formatMessage({
+                    id: "webdev.paragraph-1-text-2",
+                    defaultMessage:
+                        "Het meest voorkomende geval is website die representatieve functies vervult. Bezoekers kunnen erachter komen welke diensten biedt uw bedrijf aan, wat zijn de prijzen, waar bent u gevestigd en hoe is het best om contact met u op te nemen. Op deze website kunt u ook uw prestaties of belangrijke evenementen aankondigen. IT Context ontwerp dergelijke websites al vanaf 247 &euro; exclusief.",
+                })}
+                para3={intl.formatMessage({
+                    id: "webdev.paragraph-1-text-3",
+                    defaultMessage:
+                        "Waarschijnlijk zou u toch meer van de website verwachten. U wilt dat uw klanten makkelijk online afspraak met u kunnen maken of een offerte aanvragen. Misschien wenst u dat elke klant een persoonlijke account kunt aanmaken en daardoor een toegang krijgen tot voor hem bedoelde inhoud. Heeft u een professionele e-mailadres nodig? IT Context biedt goedkoop e-mail, de prijzen beginnen vanaf 19 &euro; exclusief per jaar. Wilt u inhoud van uw website online beheren? Met ons krijg u toegang tot Content Manager System al vanaf 49 &euro; exclusief per jaar. IT Context helpt u ook met uw eigen webshop oprichten.  Kosten hangen er af van aantal paginas, producten en gewenste functionaliteit.  Er komen nog kleine maandelijke bedragen bij voor o.a. online betalen en server capaciteit.",
+                })}
             />
             <Cards />
 
-            <Paragraph
-                heading="ipsum dolor sit"
-                para1="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Temporibus autem veritatis numquam, quidem obcaecati doloremque
-                soluta eveniet. Harum sapiente impedit illo distinctio non
-                error. Iure provident eos illo totam?"
-                para2="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Temporibus autem veritatis numquam, quidem obcaecati doloremque
-                soluta eveniet. Harum sapien?"
-                para3="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Temporibus autem veritatis numquam, quidem obcaecati doloremque
-                soluta eveniet. Harum sapiente impedit illo distinctio non
-                error. Iure provident eos illo totam?Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Temporibus autem veritatis numquam, quidem obcaecati doloremque
-                soluta eveniet. Harum sapiente impedit illo distinctio non
-                error. Iure provident eos illo totam?"
-            />
             <Intersection
                 headline={intl.formatMessage({
                     id: "webdev.intersection1-headline",
@@ -72,6 +84,15 @@ export default function Webdev() {
                 Temporibus autem veritatis numquam, quidem obcaecati doloremque
                 soluta eveniet. Harum sapiente impedit illo distinctio non
                 error. Iure provident eos illo totam?"
+            />
+            <InfoStrip
+                text="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus autem veritatis numquam, quidem obcaecati doloremque
+                soluta eveniet. Harum sapiente impedit illo distinctio non
+                error. Iure provident eos illo totam?"
+                btn="Find your solution"
+                note="with IT Context"
+                icon={<FaHandshake />}
             />
         </Container>
     );
