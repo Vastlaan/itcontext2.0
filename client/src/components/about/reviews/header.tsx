0e01d96d7d10fecc918befd03468ee228ad1e487
vastlaan <vastlaan@mail.com>
2020-09-26 19:10:13 +0200

    Add translation reviews header

--- client/src/components/about/reviews/header.tsx
+++ client/src/components/about/reviews/header.tsx
@@ -1,17 +1,33 @@
 import React from "react";
+import { useIntl } from "react-intl";
 import styled from "styled-components";
 import { fonts } from "../../../styles";
 
 export default function Header() {
+    const intl = useIntl();
+
     return (
         <Container>
-            <h3>Customers Rating</h3>
+            <h3>
+                {intl.formatMessage({
+                    id: "about.reviews-header-1",
+                    defaultMessage: "Klantenwaardering",
+                })}
+            </h3>
             <Line></Line>
             <p>
-                We listen to our customers and always try to improve based on
-                their feedback.
+                {intl.formatMessage({
+                    id: "about.reviews-header-2",
+                    defaultMessage:
+                        "We verbeteren ons werk op basis van feedback van de klanten.",
+                })}
             </p>
-            <p>Listen to what they say about us.</p>
+            <p>
+                {intl.formatMessage({
+                    id: "about.reviews-header-3",
+                    defaultMessage: "Luister naar wat ze over ons zeggen.",
+                })}
+            </p>
         </Container>
     );
 }
@@ -24,7 +40,7 @@ const Container = styled.div`
         text-shadow: 0.2rem 0.2rem 0.3rem rgba(0, 0, 0, 0.3);
     }
     p {
-        max-width: 50%;
+        max-width: 80%;
         font-family: ${fonts.cormoran};
         font-size: 2rem;
         color: ${(props) => props.theme.grey};
