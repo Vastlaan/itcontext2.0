c24cc9ca60098890fb549d0d072074383aa00989
vastlaan <vastlaan@mail.com>
2020-09-26 08:28:43 +0200

    Remove unused hooks

--- client/src/components/cookies/index.tsx
+++ client/src/components/cookies/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useIntl } from "react-intl";
 import { Link } from "react-router-dom";
