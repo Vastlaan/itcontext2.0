2f8f4b9b58262914cca510aeb021c6a092ffa888
vastlaan <vastlaan@mail.com>
2020-11-04 10:28:55 +0100

    Improve email template with inline styles font props changed

--- index.ts
+++ index.ts
@@ -134,12 +134,12 @@ app.post("/api/sendOffer", (req: Request, res: Response) => {
             }
         ],
         html: `
-             <div style=" background-image: linear-gradient(to bottom right, #0F284E, #006A93); padding: 10px; box-shadow: 0px 10px 10px rgba(0,0,0,.3); @import url('https://fonts.googleapis.com/css2?family=Forum&display=swap'); @import url('https://fonts.googleapis.com/css2?family=Forum&family=Jura:wght@400;500;600;700&display=swap');">
-                <h1 style="font-family: 'Jura', sans-serif; font-size: 39px; color: white; letter-spacing: -3px; font-weight: 700; line-height: 0; text-shadow: 5px 5px 5px rgba(0,0,0,.3);">IT Context</h1>
-                <h2 style=" font-family: 'Jura', sans-serif; font-size: 25px; color: white; letter-spacing: -3px; font-weight: 700; line-height: .5; text-shadow: 5px 5px 5px rgba(0,0,0,.3);">Web Design Studio</h2>
+             <div style=" background-image: linear-gradient(to bottom right, #0F284E, #006A93); padding: 10px; box-shadow: 0px 10px 10px rgba(0,0,0,.3);">
+                <h1 style="font-family: 'Jura', sans-serif; font-size: 39px; color: white; letter-spacing: -1px; font-weight: 600; line-height: 0; text-shadow: 5px 5px 5px rgba(0,0,0,.3);">IT Context</h1>
+                <h2 style=" font-family: 'Jura', sans-serif; font-size: 25px; color: white; letter-spacing: -1px; font-weight: 600; line-height: .5; text-shadow: 5px 5px 5px rgba(0,0,0,.3);">Web Design Studio</h2>
             </div>
-            <div style="padding: 10px; @import url('https://fonts.googleapis.com/css2?family=Forum&display=swap'); @import url('https://fonts.googleapis.com/css2?family=Forum&family=Jura:wght@400;500;600;700&display=swap');">
-                <h3 style="font-family: 'Forum', serif; font-size: 20px; color: #0F284E;">Goede dag ${email}! Hierbij sturen wij jouw offerte als bijlage.</h3>
+            <div style="padding: 10px;">
+                <h3 style="font-family: 'Forum', serif; font-size: 16px; font-weight: 400; color: #0F284E;">Goede dag ${email}! Hierbij sturen wij jouw offerte als bijlage.</h3>
                 <p style="font-family: 'Forum', serif; font-size: 16px; color: #343a40; line-height: 1.3; margin: 0;">Als je nog interesse hebt in onze diensten, aarzel niet om contact met
                 ons op te nemen via ons <a  style=" font-family: 'Forum', serif; font-size: 16px; color: #006A93; text-decoration: none;" href='mailto:info@itcontext.nl'>e-mailadress</a> of <a style=" font-family: 'Forum', serif; font-size: 16px; color: #006A93; text-decoration: none;" href='tel:0299705072'>telefoon</a>.
                 </p>
